d332d95a24a23bbd1e89228b0428f527b526222d
SiddharthNagaych <148665494+SiddharthNagaych@users.noreply.github.com>
2025-02-24 00:11:36 +0530

    Time: 0 ms (100%), Space: 43.1 MB (55.91%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,37 +1,41 @@
 class Solution {
-    private void swap(int []nums,int a ,int b){
+    public void swap(int []nums,int a,int b){
         int temp=nums[a];
         nums[a]=nums[b];
         nums[b]=temp;
-
     }
-    private void reverse(int []nums,int start){
+    public void reverse(int []nums,int start){
         int end=nums.length-1;
         while(start<end){
             swap(nums,start,end);
             start++;
             end--;
         }
+
     }
-    
     public void nextPermutation(int[] nums) {
-       // inteiallizing i with the second last index 
        int i=nums.length-2;
-       //finding the index at which the element is less than the next element
        while(i>=0&&nums[i]>=nums[i+1]){
         i--;
-       } 
-       // finding the index at which the element is just greatet than the elment at i 
-       if(i>=0){
-        int j=nums.length-1;
-        while(j>i&&nums[i]>=nums[j]){
-            j--;
-        }
-        // swaping numbers at i and j
-        swap(nums,i,j);
-
        }
-       // for minimizing the number we have to revese the arr as well
+       
+      
+       if(i>=0){
+       int j=nums.length-1;
+       while(j>i&&nums[i]>=nums[j]){
+        j--;
+       }
+       swap(nums,i,j);
+       }
+
+       
+
        reverse(nums,i+1);
+
+
+
+
+
+
     }
 }
