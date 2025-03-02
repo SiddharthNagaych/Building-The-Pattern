b7387af75ee5053cf1fa72d7f9c96e8878952e20
SiddharthNagaych <148665494+SiddharthNagaych@users.noreply.github.com>
2025-03-02 23:10:02 +0530

    Time: 5 ms (65.17%), Space: 58.3 MB (58.7%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -3,17 +3,17 @@ class Solution {
         int slow=nums[0];
         int fast=nums[0];
         do{
-            // slow is moving one step at a time and fast is moving two step at a time
+          
             slow=nums[slow];
             fast=nums[nums[fast]];
         }  while(slow !=fast);
 
         int slow1=nums[0];
-        int slow2=slow;
+       
 
-        while(slow1!=slow2){
+        while(slow1!=slow){
             slow1=nums[slow1];
-            slow2=nums[slow2];
+            slow=nums[slow];
         }
         return slow1;
     }
