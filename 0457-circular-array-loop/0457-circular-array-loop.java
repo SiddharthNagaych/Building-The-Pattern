9c7cd3af1fef96d866168486b1f87c6b68301842
SiddharthNagaych <148665494+SiddharthNagaych@users.noreply.github.com>
2025-03-04 01:18:03 +0530

    Time: 0 ms (100%), Space: 41.6 MB (23.4%) - LeetHub

--- 0457-circular-array-loop/0457-circular-array-loop.java
+++ 0457-circular-array-loop/0457-circular-array-loop.java
@@ -1,62 +1,59 @@
 class Solution {
     public boolean circularArrayLoop(int[] nums) {
-        int n=nums.length;
+       int n=nums.length;
 
-        for(int i=0;i<n;i++){
+       for(int i=0;i<nums.length;i++){
+         if(nums[i]==0){
+            continue;
+         }
+
+         int slow=i;
+         int fast=i;
+
+         boolean direction=nums[i]>0;
+
+         while(true){
+            slow=getNextJumpIdx(nums,slow,n);
+            if(nums[slow] ==0 || (nums[slow]>0) != direction){
+                break;
+            } 
+            fast=getNextJumpIdx(nums,fast,n);
+            if(nums[fast] ==0 || (nums[fast]>0) != direction){
+                break;
+            } 
+            fast=getNextJumpIdx(nums,fast,n);
+            if(nums[fast] ==0 || (nums[fast]>0) != direction){
+                break;
+            } 
+
+            if(slow == fast){
+                int nextSlow=getNextJumpIdx(nums,slow,n);
+                if(slow ==nextSlow){
+                    break;
+                }
+                return true;
+            }
+
             
-            if(nums[i]==0){
-                continue;
-            }
-            int slow =i;
-            int fast =i;
-            boolean direction =nums[i]>0;
+         }
 
-            while(true){
-                slow =getNextJumpIdx(slow,nums,n);
-                if(nums[slow]==0||(nums[slow]>0)!=direction){
-                    break;
-                }
-                fast =getNextJumpIdx(fast,nums,n);
-                if(nums[fast]==0||(nums[fast]>0)!=direction){
-                    break;
-                }
-                fast =getNextJumpIdx(fast,nums,n);
-                if(nums[fast]==0||(nums[fast]>0)!=direction){
-                    break;
-                }
+          slow =i;
+          while(nums[slow] != 0&& (nums[slow]>0) == direction){
+             int nextSlow=getNextJumpIdx(nums,slow,n);
+             nums[slow]=0;
+             slow=nextSlow;
+          }
 
-                if(slow == fast){
-                    int nextSlow =getNextJumpIdx(slow,nums,n);
-                    if(slow == nextSlow){
-                        break;
-                    }
-                    return true;
-                }
-
-               
-                
-            }
-               slow =i;
-
-                while(nums[slow] != 0 && (nums[slow]>0)==direction ){
-                    int nextSlow=getNextJumpIdx(slow,nums,n);
-                    nums[slow]=0;
-                    slow =nextSlow;
-                }
-
-
-        }
-
-        return false;
-
-        
+       }
+       return false;
     }
 
-    public int getNextJumpIdx(int current , int [] nums,int n){
-        int next =(current+nums[current])%n;
+    public int getNextJumpIdx(int []nums,int current,int n){
+        int next=(current+nums[current])%n;
         if(next<0){
             next +=n;
         }
+
         return next;
     }
 }
