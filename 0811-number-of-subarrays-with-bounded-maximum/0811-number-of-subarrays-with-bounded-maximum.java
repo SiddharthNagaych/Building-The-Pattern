6ed809b93a3654f7a9df83a2528ab2fade15bb83
SiddharthNagaych <148665494+SiddharthNagaych@users.noreply.github.com>
2025-03-05 00:09:08 +0530

    Time: 4 ms (23.13%), Space: 56.8 MB (49.83%) - LeetHub

--- 0811-number-of-subarrays-with-bounded-maximum/0811-number-of-subarrays-with-bounded-maximum.java
+++ 0811-number-of-subarrays-with-bounded-maximum/0811-number-of-subarrays-with-bounded-maximum.java
@@ -1,20 +1,20 @@
 class Solution {
-    public int countSubArrays(int[] nums, int  bound){
-        int count=0;
+    
+    public int countMaxSubArray(int[] nums, int bound ){
+        int count =0;
         int currentCount=0;
         for(int i:nums){
-           
             if(i<=bound){
                 currentCount++;
                 count +=currentCount;
             }else{
-
-            currentCount=0;
+                currentCount=0;
             }
         }
+
         return count;
     }
     public int numSubarrayBoundedMax(int[] nums, int left, int right) {
-        return countSubArrays(nums,right)-countSubArrays(nums,left-1);
+       return countMaxSubArray(nums,right)-countMaxSubArray(nums,left-1);
     }
 }
