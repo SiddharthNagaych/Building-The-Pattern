9300e9338f9d9a2b9495594bfac2019b1e3c0561
SiddharthNagaych <148665494+SiddharthNagaych@users.noreply.github.com>
2025-03-06 03:05:58 +0530

    Time: 7 ms (94.11%), Space: 44.7 MB (39.19%) - LeetHub

--- 0719-find-k-th-smallest-pair-distance/0719-find-k-th-smallest-pair-distance.java
+++ 0719-find-k-th-smallest-pair-distance/0719-find-k-th-smallest-pair-distance.java
@@ -1,29 +1,31 @@
-public class Solution {
+class Solution {
     public int smallestDistancePair(int[] nums, int k) {
         Arrays.sort(nums);
-        int n = nums.length;
-        int low = 0;
-        int high = nums[n - 1] - nums[0];
-        
-        while (low < high) {
-            int mid = low + (high - low) / 2;
-            int count = 0, left = 0;
-            
-           
-            for (int right = 0; right < n; right++) {
-                while (nums[right] - nums[left] > mid) {
+        int n=nums.length;
+
+        int minimumDistance=0;
+        int maximumDistance=nums[n-1]-nums[0];
+
+        while(minimumDistance<maximumDistance){
+            int midDistance=minimumDistance +(maximumDistance-minimumDistance)/2;
+
+            int left=0;
+            int count=0;
+
+            for(int right=0;right<n;right++){
+                while(nums[right]-nums[left]>midDistance){
                     left++;
                 }
-                count += right - left;
+                count +=right-left;
             }
-            
-            if (count < k) {
-                low = mid + 1;
-            } else {
-                high = mid;
+
+            if(count<k){
+             minimumDistance =midDistance+1;
+            }else{
+                maximumDistance=midDistance;
             }
         }
-        
-        return high;
+
+        return minimumDistance;
     }
 }
