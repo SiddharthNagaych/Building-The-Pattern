a7c8e30b9b74ba7c4e597389cae1f5a42aa64757
SiddharthNagaych <148665494+SiddharthNagaych@users.noreply.github.com>
2025-03-11 00:11:06 +0530

    Time: 7 ms (93.49%), Space: 80 MB (21.5%) - LeetHub

--- 1972-rotating-the-box/1972-rotating-the-box.java
+++ 1972-rotating-the-box/1972-rotating-the-box.java
@@ -2,35 +2,31 @@ class Solution {
     public char[][] rotateTheBox(char[][] boxGrid) {
         int m=boxGrid.length;
         int n=boxGrid[0].length;
+        char [][] res=new char[n][m];
 
-        char [][] rotatedBox=new char[n][m];
-
-        // lets apply the gravity
+        // applyting the gravity in the original arr
 
         for(int i=0;i<m;i++){
-            int emptyspaceIdx=n-1;
+            int emptySpace=n-1;
             for(int j=n-1;j>=0;j--){
-               if(boxGrid[i][j]=='#'){
-                boxGrid[i][j]='.';
-                boxGrid[i][emptyspaceIdx]='#';
-                emptyspaceIdx--;
-
-               }else if(boxGrid[i][j]=='*'){
-                emptyspaceIdx=j-1;
-               }
+                if(boxGrid[i][j]=='#'){
+                    boxGrid[i][j]='.';
+                    boxGrid[i][emptySpace]='#';
+                    emptySpace--;
+                }else if(boxGrid[i][j]=='*'){
+                    emptySpace=j-1;
+                }
             }
         }
 
-        // Now lets rotate the matrix by 90 degree
+        // rotating the arr
 
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-               rotatedBox[j][m-1-i]=boxGrid[i][j];
+                res[j][m-1-i]=boxGrid[i][j];
             }
         }
 
-        return rotatedBox;
-
-
+        return res;
     }
 }
