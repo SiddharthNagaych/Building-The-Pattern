2162a650a411d2106c093d594a6d6bbfda61f388
SiddharthNagaych <148665494+SiddharthNagaych@users.noreply.github.com>
2025-03-14 01:12:58 +0530

    Time: 18 ms (24.89%), Space: 46.7 MB (48.89%) - LeetHub

--- 1133-last-substring-in-lexicographical-order/1133-last-substring-in-lexicographical-order.java
+++ 1133-last-substring-in-lexicographical-order/1133-last-substring-in-lexicographical-order.java
@@ -1,23 +1,21 @@
-public class Solution {
+class Solution {
     public String lastSubstring(String s) {
-        int n = s.length();
-        int i = 0; // Pointer for the first character of the result substring
-        int j = 1; // Pointer for the current character being compared
-        int k = 0; // Length of the common prefix
+        int i=0;
+        int j=1;
+        int k=0;
 
-        while (j + k < n) {
-            if (s.charAt(i + k) == s.charAt(j + k)) {
-                // If characters are the same, move to the next character
+        int n=s.length();
+
+        while(j+k<n){
+            if(s.charAt(i+k)==s.charAt(j+k)){
                 k++;
-            } else if (s.charAt(i + k) < s.charAt(j + k)) {
-                // If the current character at j is larger, update i to j
-                i = Math.max(i + k + 1, j);
-                j = i + 1;
-                k = 0;
-            } else {
-                // If the current character at j is smaller, move j forward
-                j = j + k + 1;
-                k = 0;
+            }else if(s.charAt(i+k)<s.charAt(j+k)){
+                i=Math.max(i+k+1,j);
+                j=i+1;
+                k=0;
+            }else{
+                j++;
+                k=0;
             }
         }
 
