83fc7540a71fe0d3f9986ef5c816d70295d55af1
SiddharthNagaych <148665494+SiddharthNagaych@users.noreply.github.com>
2025-03-15 22:20:17 +0530

    Time: 1 ms (21.98%), Space: 47.3 MB (5.85%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
@@ -1,30 +1,31 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        int left = 0;
-        int right = 1;
-        int idx = 0;
-        int n = nums.length;
+        int left=0;
+        int right=1;
+        int idx=0;
+        int n=nums.length;
 
-        while (right < n) {
-            if (nums[left] == nums[right]) {
-                right++; // Move right pointer if duplicates are found
-            } else {
-                // Copy at most two instances of the current element
-                int count = Math.min(2, right - left); // Allow at most 2 duplicates
-                for (int i = 0; i < count; i++) {
-                    nums[idx++] = nums[left];
+        while(right<n){
+            if(nums[left]==nums[right]){
+                right++;
+            }else{
+                int count=Math.min(2,right-left);
+
+                for(int i=0;i<count;i++){
+                    nums[idx++]=nums[left];
                 }
-                left = right; // Move left to the new element
+                left=right;
                 right++;
             }
         }
 
-        // Handle the last set of duplicates
-        int count = Math.min(2, right - left); // Allow at most 2 duplicates
-        for (int i = 0; i < count; i++) {
-            nums[idx++] = nums[left];
+        int count=Math.min(2,right-left);
+        for(int i=0;i<count;i++){
+            nums[idx++]=nums[left];
         }
 
-        return idx; // Return the new length of the array
+        return idx;
+
+
     }
 }
