600b37ca8896a338f6f07903c17cad4e96c948d5
SiddharthNagaych <148665494+SiddharthNagaych@users.noreply.github.com>
2025-03-18 01:06:48 +0530

    Time: 1 ms (98.23%), Space: 45 MB (30.03%) - LeetHub

--- 1168-duplicate-zeros/1168-duplicate-zeros.java
+++ 1168-duplicate-zeros/1168-duplicate-zeros.java
@@ -1,25 +1,22 @@
 class Solution {
     public void duplicateZeros(int[] arr) {
-        int n = arr.length;
-        int zeroCount = 0;
-
-        // Count the number of zeros to determine the shift
-        for (int num : arr) {
-            if (num == 0) {
-                zeroCount++;
+        int zeroCount=0;
+        int n=arr.length;
+        for(int i:arr){
+            if(i==0){
+               zeroCount++;
             }
         }
-
-        // Iterate from the end to the beginning
-        for (int i = n - 1; i >= 0; i--) {
-            if (i + zeroCount < n) {
-                arr[i + zeroCount] = arr[i];
+        for(int i=n-1;i>=0;i--){
+            if(i+zeroCount<n){
+                arr[i+zeroCount]=arr[i];
             }
-            if (arr[i] == 0) {
+
+            if(arr[i]==0){
                 zeroCount--;
-                if (i + zeroCount < n) {
-                    arr[i + zeroCount] = 0;
-                }
+                if(i+zeroCount<n){
+                arr[i+zeroCount]=0;
+            }
             }
         }
     }
