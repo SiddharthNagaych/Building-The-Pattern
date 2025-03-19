7a86ced64d92e601fe3c84f3caf3f6cc291334d5
SiddharthNagaych <148665494+SiddharthNagaych@users.noreply.github.com>
2025-03-19 13:07:01 +0530

    Time: 1 ms (97.8%), Space: 44.2 MB (5.49%) - LeetHub

--- 1183-statistics-from-a-large-sample/1183-statistics-from-a-large-sample.java
+++ 1183-statistics-from-a-large-sample/1183-statistics-from-a-large-sample.java
@@ -1,10 +1,10 @@
 class Solution {
     public double[] sampleStats(int[] count) {
-        int max=Integer.MIN_VALUE;
         int min=Integer.MAX_VALUE;
-        long sum =0;
+        int max=Integer.MIN_VALUE;
         int totalCount=0;
         int maxFreq=0;
+        long sum =0;
         int mode=0;
 
         for(int i=0;i<count.length;i++){
@@ -15,35 +15,37 @@ class Solution {
                 if(i<min){
                     min=i;
                 }
-                sum += (long)i*count[i];
+
+                sum += (long)count[i]*i;
                 totalCount += count[i];
+
                 if(maxFreq<count[i]){
                     maxFreq=count[i];
                     mode=i;
-
                 }
 
 
             }
         }
+        double mean=(double)sum/totalCount;
 
-        double mean =(double)sum/totalCount;
-
-        // lets Start calculateing medina as well 
-
-
-        double median=0;
+        // lets calculate the median as well
 
         int currentCount=0;
         int medianPos=(totalCount+1)/2;
         boolean isEven=totalCount%2==0;
+        double median=0;
+
+        
+
 
         for(int i=0;i<count.length;i++){
             if(count[i]>0){
-                currentCount += count[i];
+                currentCount +=count[i];
+
                 if(currentCount>=medianPos){
-                    if(!isEven || currentCount>medianPos){
-                        median =i;
+                    if(!isEven||currentCount>medianPos){
+                        median=i;
                     }else{
                         for(int j=i+1;j<count.length;j++){
                             if(count[j]>0){
@@ -54,12 +56,9 @@ class Solution {
                     }
                     break;
                 }
+
             }
         }
-
         return new double[]{min,max,mean,median,mode};
-
-
-
     }
 }
