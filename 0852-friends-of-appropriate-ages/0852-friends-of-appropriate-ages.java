42d84096a4fbf931ab001607ae99315861b5ac0d
SiddharthNagaych <148665494+SiddharthNagaych@users.noreply.github.com>
2025-04-25 06:51:54 +0530

    Time: 37 ms (8.55%), Space: 48.4 MB (8.08%) - LeetHub

--- 0852-friends-of-appropriate-ages/0852-friends-of-appropriate-ages.java
+++ 0852-friends-of-appropriate-ages/0852-friends-of-appropriate-ages.java
@@ -1,13 +1,64 @@
 class Solution {
+    // Find the index of the first element > num
+    public int upperBound(int[] ages, int num) {
+        int low = 0;
+        int high = ages.length - 1;
+        
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (ages[mid] <= num) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        
+        return low;
+    }
+    
+    // Find the index of the first element >= num
+    public int lowerBound(int[] ages, int num) {
+        int low = 0;
+        int high = ages.length - 1;
+        
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (ages[mid] < num) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        
+        return low;
+    }
+    
     public int numFriendRequests(int[] ages) {
         Arrays.sort(ages);
-        int left = 0, i = 0, result = 0, prev = 0;
-        for(i = 1; i < ages.length; i++) {
-            while(left < i && ages[left] <= 0.5*ages[i]+7) left++;
-            while(prev < i && ages[i] != ages[prev]) prev++;
-            if(ages[i] == ages[prev] && ages[i] > 0.5*ages[i]+7) result+=i-prev;
-            result+=i-left;
+        
+        int res = 0;
+        for (int i = 0; i < ages.length; i++) {
+            int currAge = ages[i];
+            // Based on the age rule: age[y] <= 0.5 * age[x] + 7 (not allowed)
+            // So we want age[y] > 0.5 * age[x] + 7
+            int lowerLimit = (int) (0.5 * currAge + 7) + 1;
+            
+            // Find the count of people within valid age range
+            int lower = lowerBound(ages, lowerLimit);
+            int upper = upperBound(ages, currAge);
+            
+            // Calculate number of valid requests for this person
+            int validRequests = Math.max(0, upper - lower);
+            
+            // If we're counting from the same position as current age,
+            // don't count request to self
+            if (lower <= i && i < upper) {
+                validRequests--;
+            }
+            
+            res += validRequests;
         }
-        return result;
+        
+        return res;
     }
 }
