e33cf8177cbbdc808685c5958f536d0bebfa847c
SiddharthNagaych <148665494+SiddharthNagaych@users.noreply.github.com>
2025-09-15 10:43:15 +0530

    Time: 3 ms (63.99%), Space: 45.4 MB (38.05%) - LeetHub

--- 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.java
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.java
@@ -1,44 +1,26 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
-    public boolean bfs(TreeNode root, int k,HashSet<Integer> hs,Queue<TreeNode> q){
-        if(root ==null) return false;
-
-        q.offer(root);
-
-        while(!q.isEmpty()){
-            TreeNode curr=q.poll();
-            int diff=k-curr.val;
-
-            if(hs.contains(diff)){
-                return true;
-            }
-
-            hs.add(curr.val);
-
-            if(curr.left !=null) q.offer(curr.left);
-            if(curr.right !=null) q.offer(curr.right);
-
-        }
-        return false;
-    }
     public boolean findTarget(TreeNode root, int k) {
-        Queue<TreeNode> q=new LinkedList<>();
-        HashSet<Integer> hs=new HashSet<>();
-        return bfs(root,k,hs,q);
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        return inorderHelper(root, k, hm);
+    }
     
+    public boolean inorderHelper(TreeNode root, int k, HashMap<Integer, Integer> hm) {
+        if (root == null) return false;
+        
+        // First traverse left subtree
+        if (inorderHelper(root.left, k, hm)) {
+            return true;
+        }
+        
+        // Check if complement exists in HashMap
+        if (hm.containsKey(root.val)) {
+            return true;
+        }
+        
+        // Add current value to HashMap
+        hm.put(k-root.val, 1);
+        
+        // Then traverse right subtree
+        return inorderHelper(root.right, k, hm);
     }
 }
